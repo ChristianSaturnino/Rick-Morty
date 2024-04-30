aa071495d21dde31880cdf846eaab81b0b9c40e4
Christian Saturnino <christian.saturnino95@hotmail.com>
2024-04-30 19:39:48 -0300

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>Christian Saturnino Andrade Oliveira</h1>
+    <img id="img-sobre" src="C:\Users\chris\OneDrive\Área de Trabalho\Estudos\TrabalhosFaculdade\RickMorty\RickMorty\src\assets\EU MENO.png" alt="">
+    <h1 class="title">Christian Saturnino Andrade Oliveira</h1>
+    <div class="texto">
+      <h3 id="dados">19/08/2004 - ADS - Marilia-SP</h3>
+      <p>Aluno em Analise e Desenvolvimento de sistemas e Estagiário em Desenvolvimento na Pedbot.<br/>
+
+Possuo conhecimentos em Python, PHP, C#, HTML, CSS , JS e toda a parte de banco de dados utilizando o mySQL e o SQL Server, e também possuo um breve conhecimento sobre nuvem (GCP).<br/>
+
+Além disso, possuo conhecimento em Docker, NodeJS com express e React.<br/>
+
+Trabalho muito bem em equipe e possuo forte espirito de liderança, gosto muito de aprender,sou bastante dedicado e busco sempre o melhor em tudo que eu faço.</p>
+    </div>
   </div>
 </template>
 
@@ -8,15 +19,28 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    justify-content: center;
+    align-items: center;  
+    margin-top: 30px;
   }
 }
 
 .about{
-  justify-content: center;
-  align-items: center
+  text-align: center;
+  color: white;
 }
+
+#dados {
+  margin-bottom: 30px;
+}
+
+#img-sobre {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+}
+
+
 </style>
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@ onMounted(() => {
 
   });
 });
-// });
+
 </script>
 
 <template>
@@ -22,7 +22,7 @@ onMounted(() => {
     <div class="row m-3">
       
   <div class="col-md-6" v-for="character in characters" :key="character.id">
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="card mb-3" style="max-width: 660px;">
       <div class="row g-0">
         <div class="col-md-4">
           <img :src="character.image" height="100%" width="100%" class="card-img-top" alt="...">
@@ -32,9 +32,10 @@ onMounted(() => {
             <h5 class="card-title">{{ character.name }}</h5>
             <p class="card-text">Status: {{ character.status }}</p>
             <p class="card-text">Species: {{ character.species }}</p>
-            <p class="card-text">{{ character.location.name }}</p>
+            <p class="card-text">Location: {{ character.location.name }}</p>
+            <p class="card-text text-white">Episodes: {{ character.episode.length }}</p>
             <p class="card-text "><small class="text-body-white">Gender: {{ character.gender }}</small></p>
-            <p class="card-text text-white">{{ character.episode.length }}</p>
+
           </div>
         </div>
       </div>
